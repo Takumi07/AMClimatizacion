238803791bee8d350ecc1584cf39f3858d6c58eb
Takumi07 <ALEXIS_TAKUMI@HOTMAIL.COM>
2016-04-04 21:01:37 -0300

    201604040901

--- AMClimatizacion/default.aspx.vb
+++ AMClimatizacion/default.aspx.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Response.Redirect("EnConstruccion.aspx")
+        Response.Redirect("EnConstruccion3.aspx")
     End Sub
 
 End Class
