0ea65dff26c9848e037e013fb651901c588ba8f6
Alexis Yañez <ALEXIS_TAKUMI@HOTMAIL.COM>
2016-06-04 14:23:01 -0300

    201606041422

--- AMClimatizacion/default.aspx.vb
+++ AMClimatizacion/default.aspx.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-        Response.Redirect("index.aspx")
+        Response.Redirect("enConstruccion.aspx")
     End Sub
 
 End Class
