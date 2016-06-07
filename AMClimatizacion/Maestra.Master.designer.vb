d47658c3fa976ec9227219e5d54427ddf40609f7
SantaT89 <santa.tassara@gmail.com>
2016-06-07 14:46:00 -0300

    07/06/2016

--- AMClimatizacion/Maestra.Master.designer.vb
+++ AMClimatizacion/Maestra.Master.designer.vb
@@ -12,7 +12,7 @@ Option Explicit On
 
 
 Partial Public Class Maestra
-
+    
     '''<summary>
     '''Control head.
     '''</summary>
@@ -21,7 +21,7 @@ Partial Public Class Maestra
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents head As Global.System.Web.UI.WebControls.ContentPlaceHolder
-
+    
     '''<summary>
     '''Control form4.
     '''</summary>
@@ -30,7 +30,16 @@ Partial Public Class Maestra
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents form4 As Global.System.Web.UI.HtmlControls.HtmlForm
-
+    
+    '''<summary>
+    '''Control logout.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents logout As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+    
     '''<summary>
     '''Control subirTrabajos.
     '''</summary>
@@ -39,16 +48,7 @@ Partial Public Class Maestra
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents subirTrabajos As Global.System.Web.UI.HtmlControls.HtmlGenericControl
-
-    '''<summary>
-    '''Control Logout.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents Logout As Global.System.Web.UI.HtmlControls.HtmlGenericControl
-
+    
     '''<summary>
     '''Control login.
     '''</summary>
@@ -57,7 +57,7 @@ Partial Public Class Maestra
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents login As Global.System.Web.UI.HtmlControls.HtmlGenericControl
-
+    
     '''<summary>
     '''Control contenidoPagina.
     '''</summary>
--- BLL/MailingBLL.vb
+++ BLL/MailingBLL.vb
@@ -8,12 +8,22 @@
             'A quien va
             Correo.To.Add(New System.Net.Mail.MailAddress("ALEXIS.TAKUMI@GMAIL.COM"))
             Correo.Subject = "AM Climatización - Solicitud de Contacto"
-            Correo.Body = "<html><head> </head><body><img src=""http://i64.tinypic.com/wmkspg.jpg"" width=""380px"" height=""100""/><br/><br/><b> " &
-            "Solicitud de Contacto</b><br/><hr " &
-            " style=""border-style: 0; border-color: 0; border-width: 0px; padding: 0px; margin: 0px; height: 7px; background-color: #3593D9;"" /> " &
-            " <br /><span><b><u>Nombre y Apellido: </u></b> " & paramNombreApellido & " <br/> <b><u>Correo: </u></b> " & paramCorreo & " <br/>  <b><u>Teléfono: </u></b> " & paramTelefono & " <br/>  <b><u>Mensaje: </u></b> " & paramMensaje & "</span><hr " &
-            "style=""border-style: 0; border-color: 0; border-width: 0px; padding: 0px; margin: 0px; height: 7px; background-color: #3593D9;"" /> " &
-            " </body></html> "
+            ' Correo.Body = "<html><head> </head><body><img src=""http://i64.tinypic.com/wmkspg.jpg"" width=""380px"" height=""100""/><br/><br/><b> " &
+            '    "Solicitud de Contacto</b><br/><hr " &
+            '    " style=""border-style: 0; border-color: 0; border-width: 0px; padding: 0px; margin: 0px; height: 7px; background-color: #3593D9;"" /> " &
+            '    " <br /><span><b><u>Nombre y Apellido:</u> </b> " & paramNombreApellido & " <br/> <b><u>Correo:</u> </b> " & paramCorreo & " <br/>  <b><u>Teléfono:</u> </b> " & paramTelefono & " <br/>  <b><u>Mensaje:</u> </b> " & paramMensaje & "</span><br/><hr " &
+            '      "style=""border-style: 0; border-color: 0; border-width: 0px; padding: 0px; margin: 0px; height: 7px; background-color: #3593D9;"" /> " &
+            '       " </body></html> "
+
+
+            Correo.Body = "<html><head> </head><body>
+<img src=""http://i64.tinypic.com/wmkspg.jpg"" alt="" width=""327"" height=""64"" />
+<hr style=""height 3px; background-color: #3593d9;"" />
+<p style=""text-align center;""><span style=""font-size: 16px; font-weight: bold; color: #3593d9;"">Solicitud de Contacto</span> </p>
+<hr style=""height 3px; background-color: #3593d9;"" />
+<p><br /> <span style=""font-size 12px;""><strong><span style=""text-decoration: underline;"">Nombre y Apellido:</span> </strong> " & paramNombreApellido & " <br /> <br /> <strong><span style=""text-decoration: underline;"">Correo:</span> </strong> " & paramCorreo & " <br /> <br /> <strong><span style=""text-decoration: underline;"">Tel&eacute;fono:</span> </strong> " & paramTelefono & " <br /> <br /> <strong><span style=""text-decoration: underline;"">Mensaje:</span> </strong> " & paramMensaje & "</span> <br /> </p>
+<hr style=""height 3px; background-color: #3593d9;"" />
+</body></html> "
             Correo.Priority = System.Net.Mail.MailPriority.Normal
             Dim smtp As New System.Net.Mail.SmtpClient
             'Modificado para que envie desde la web
